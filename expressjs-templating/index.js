89048ef361b03c1751864c16f6645a408b9719c7
Zhivko Shopov <zhishopov@gmail.com>
2025-04-05 12:08:31 +0100

    Add Middleware

--- expressjs-templating/index.js
+++ expressjs-templating/index.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+// Middleware
+app.use("/auth", (req, res, next) => {
+  if (Math.random() < 0.5) {
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
@@ -14,6 +23,10 @@ app.get("/cats/:catName", (req, res) => {
   const catName = req.params.catName;
 
   res.send(`<h1>GLORIOUS: ${catName}`);
+});
+
+app.get("/auth", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(2000, () =>
